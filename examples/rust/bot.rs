8b4a595ccb1d904d61625c1ebe6fffbe282b552d
seb <27khero@proton.me>
2024-12-23 19:44:55 -0300

    :p

---
+++ examples/rust/bot.rs
@@ -0,0 +1,37 @@
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("Logged in as {}", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = "YOUR_TOKEN_HERE";
+
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
