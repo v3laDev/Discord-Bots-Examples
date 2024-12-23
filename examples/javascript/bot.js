f6a07310768f68098c0f9ac407de9838eae9c5db
seb <27khero@proton.me>
2024-12-23 19:46:57 -0300

    :p

---
+++ examples/javascript/bot.js
@@ -0,0 +1,19 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+// Event: Bot is ready
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+// Event: Message received
+client.on('messageCreate', (message) => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+// Login to Discord with your bot's token
+client.login('YOUR_TOKEN_HERE');
